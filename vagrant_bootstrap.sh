d4c02e053bc207bbc6b020f2509c8ae6d360a805
TheBirdie <ProjetSigma>
2015-12-24 10:30:42 +0100

    [vagrant/frontend] Fix 'npm install'. [vagrant/backend] Use prod and dev requirements

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -12,15 +12,19 @@ sudo apt-get -yqq install git python3-pip python-dev libmysqlclient-dev libjpeg-
 
 cd /vagrant/backend
 echo 'Installing pip dependencies...'
-sudo pip3 install -r requirements.txt
+sudo pip3 install -r requirements/dev.txt
+sudo pip3 install -r requirements/prod.txt
 echo "Running migrations..."
 python3 manage.py migrate
 
 cd /vagrant/frontend
+# Remove old modules from previous install / host machine
+sudo rm -rf node_modules
 sudo npm install -g tsd gulp
-npm install --loglevel=info
+sudo -H -u vagrant npm install --loglevel=info
 
 echo ""
 echo "Vagrant install complete."
 echo "Now try logging in:"
 echo "    $ vagrant ssh"
+
