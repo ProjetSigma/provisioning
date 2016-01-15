ffe950542f60e571c642a5f892c3bc098daf241d
TheBirdie <Papaya>
2016-01-15 17:03:07 +0100

    Front: front working again

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -5,15 +5,22 @@
 # echo 'Acquire::http::Proxy "http://129.104.247.2:8080/";' >> /etc/apt/apt.conf
 
 export DEBIAN_FRONTEND=noninteractive
-echo 'Installing required packages...'
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-DISTRO="vivid"
-sudo sh -c "echo 'deb https://deb.nodesource.com/node_5.x ${DISTRO} main' > /etc/apt/sources.list.d/nodesource.list"
-sudo sh -c "echo 'deb-src https://deb.nodesource.com/node_5.x ${DISTRO} main' >> /etc/apt/sources.list.d/nodesource.list"
+export LINE_BREAK_UP="
+
+###########################################
+#"
+export LINE_BREAK="#
+###########################################"
+
+
+######################## BACK END ######################################
+echo "${LINE_BREAK_UP}"
+echo '# 1/6 Backend setup - apt packages'
+echo "${LINE_BREAK}"
 
 sudo apt-get update
 sudo apt-get -yqq install git python3-pip python-dev libmysqlclient-dev libjpeg-dev \
-  build-essential apt-transport-https nodejs
+  build-essential apt-transport-https
 
 cd /vagrant/backend
 echo "from .settings_default import *
@@ -24,18 +31,49 @@ class contains_all_list(list):
     def __contains__(self, key):
         return True
 
-INTERNAL_IPS = contains_all_list()" >> sigma/settings.py
-echo 'Installing pip dependencies...'
-sudo pip3 install -r requirements/dev.txt
-sudo pip3 install -r requirements/prod.txt
-echo "Running migrations..."
+INTERNAL_IPS = contains_all_list()" > sigma/settings.py
+
+echo "${LINE_BREAK_UP}"
+echo '# 2/6 Backend setup - pip requirements'
+echo "${LINE_BREAK}"
+
+sudo chown -R vagrant:vagrant /vagrant/backend
+pip3 install -r requirements/dev.txt
+pip3 install -r requirements/prod.txt
+
+echo "${LINE_BREAK_UP}"
+echo '# 3/6 Backend setup - Migrations'
+echo "${LINE_BREAK}"
+
 python3 manage.py migrate
+
+
+######################## FRONT END ######################################
+echo "${LINE_BREAK_UP}"
+echo '# 4/6 Frontend setup - apt packages'
+echo "${LINE_BREAK}"
+
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+DISTRO="vivid"
+sudo sh -c "echo 'deb https://deb.nodesource.com/node_5.x ${DISTRO} main' > /etc/apt/sources.list.d/nodesource.list"
+sudo sh -c "echo 'deb-src https://deb.nodesource.com/node_5.x ${DISTRO} main' >> /etc/apt/sources.list.d/nodesource.list"
+apt-get install -yqq nodejs
 
 cd /vagrant/frontend
 # Remove old modules from previous install / host machine
 sudo rm -rf node_modules
-sudo npm install -g tsd gulp
-sudo -H -u vagrant npm install --loglevel=info
+
+
+echo "${LINE_BREAK_UP}"
+echo '# 5/6 Frontend setup - TSD & GULP'
+echo "${LINE_BREAK}"
+sudo HTTPS_PROXY=$HTTPS_PROXY npm install -g tsd gulp
+
+
+echo "${LINE_BREAK_UP}"
+echo '# 6/6 Frontend setup - npm packages'
+echo "${LINE_BREAK}"
+sudo -H -u vagrant HTTPS_PROXY=$HTTPS_PROXY npm install --loglevel=info
 
 echo ""
 echo "Vagrant install complete."
